6ea9ce856ad81a4f8a1776bfb304d3f6ab7500c6
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-03 15:43:47 -0500

    created login page

--- journey-joiner/src/App.jsx
+++ journey-joiner/src/App.jsx
@@ -8,12 +8,14 @@ import {
 import Layout from "../src/components/Layout"
 import Home from "../src/pages/Home"
 import Signup from "../src/pages/Signup"
+import Login from "../src/pages/Login"
 
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="signup" element={<Signup />} />
+    <Route path="login" element={<Login />} />
   </Route>
 ))
 
---
+++ journey-joiner/src/pages/Login.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+// import logo from '../assets/logo.png'
+
+export default function Login() {
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here you would usually send the user data to the backend
+        console.log(user);
+    };
+
+    return (
+        <main className="main-background">
+            <div className="signup-container">
+                <h2>Log In</h2>
+                {/* <img src={logo} alt="Journey Joiner Logo" /> */}
+                <form className="signup-form" onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        onChange={handleChange}
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        placeholder="Password"
+                        required
+                    />
+                    <button type="submit">Log In</button>
+                </form>
+                <br />
+                <p>Don't have an account? <NavLink to="/signup">Sign Up</NavLink></p>
+            </div>
+        </main>
+    );
+}
