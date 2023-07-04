07ede5af80b524f7973845b3641b7207e6318450
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-04 14:55:39 -0500

    created TravelCardSmall comp

---
+++ journey-joiner/src/components/TravelCardSmall.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import defaultProfileImage from '../assets/blank-profile.png';
+import beachBumImage from "../assets/beach-bum.png"
+import foodieImage from "../assets/foodie.png"
+import adventurerImage from "../assets/adventurer.png"
+import museumMagnetImage from "../assets/museum-magnet.png"
+
+
+export default function TravelCardSmall({ profileData }) {
+
+  const renderInterestImages = () => {
+    return profileData.interests.map((interest) => {
+      switch (interest) {
+        case 'beach bum':
+          return <img src={beachBumImage} alt="Beach Bum" width="15" height="15" />;
+        case 'foodie':
+          return <img src={foodieImage} alt="Foodie" width="15" height="15" />;
+        case 'adventurer':
+          return <img src={adventurerImage} alt="Adventurer" width="15" height="15" />;
+        case 'museum magnet':
+          return <img src={museumMagnetImage} alt="Museum Magnet" width="15" height="15" />;
+        default:
+          return null;
+      }
+    });
+  };
+
+
+  return (
+    <div className="travel-card-small-container">
+      <div className="travel-card-small-left">
+        <div className="travel-card-small-profile-picture">
+          <img src={profileData.profilePicture || defaultProfileImage} alt="Profile" />
+        </div>
+        <div className="travel-card-small-data-left">
+          <p id="travel-card-small-name">{profileData.firstName}, {profileData.age}</p>
+          <p id="travel-card-small-budget">Budget: {profileData.budget}/day</p>
+        </div>
+      </div>
+      <div className="travel-card-small-right">
+        <div className="travel-card-small-bio">
+          <p>Bio: {profileData.bio}</p>
+        </div>
+
+        <div className="travel-card-small-interests">
+          {renderInterestImages()}
+        </div>
+      </div>
+    </div>
+  );
+}
--- journey-joiner/src/pages/Matches.jsx
+++ journey-joiner/src/pages/Matches.jsx
@@ -1,19 +1,51 @@
 import React, { useState } from 'react';
 import Dashboard from '../components/Dashboard';
+import TravelCardSmall from '../components/TravelCardSmall';
 import defaultProfileImage from '../assets/blank-profile.png';
 import logo from '../assets/logo.png';
 
 export default function Matches() {
+  const profileData = {
+    profilePicture: defaultProfileImage,
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    budget: '$300',
+    interests: ['beach bum', 'foodie', 'adventurer', 'museum magnet'],
+  };
 
   return (
     <main className="main-background">
       <Dashboard />
-      <section className="discover-container">
-        <div className="discover-container-title">
-          <img src={logo} alt="Journey Joiner Logo" width="70px" />
-          <h2>Discover: Match or Pass</h2>
+      <div className="matches-messages-container">
+
+        <section className="matches-container">
+
+            <div className="matches-container-title">
+            <img src={logo} alt="Journey Joiner Logo" width="70px" />
+            <h2>Matches</h2>
+            </div>
+
+        </section>
+
+        <section className="messages-container">
+
+            <div className="matches-container-title">
+            <img src={logo} alt="Journey Joiner Logo" width="70px" />
+            <h2>Messages</h2>
+            </div>
+            
+            <TravelCardSmall profileData={profileData} />
+            <div className="direct-messaging">
+            {/* Direct messaging component */}
+            </div>
+
+        </section>
+
         </div>
-      </section>
     </main>
   );
 }
