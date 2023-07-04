2fdc3d45a0578c8cfd47806ce82141ba4129179d
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-04 12:16:25 -0500

    styling

--- journey-joiner/src/components/TravelCard.jsx
+++ journey-joiner/src/components/TravelCard.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import defaultProfileImage from '../assets/blank-profile.png';
+import beachBumImage from "../assets/beach-bum.png"
+import foodieImage from "../assets/foodie.png"
+import adventurerImage from "../assets/adventurer.png"
+import museumMagnetImage from "../assets/museum-magnet.png"
 
-export default function ProfileContainer({ profileData }) {
+
+export default function TravelCard({ profileData }) {
+
+  const renderInterestImages = () => {
+    return profileData.interests.map((interest) => {
+      switch (interest) {
+        case 'beach bum':
+          return <img src={beachBumImage} alt="Beach Bum" width="50" height="50" />;
+        case 'foodie':
+          return <img src={foodieImage} alt="Foodie" width="50" height="50" />;
+        case 'adventurer':
+          return <img src={adventurerImage} alt="Adventurer" width="50" height="50" />;
+        case 'museum magnet':
+          return <img src={museumMagnetImage} alt="Museum Magnet" width="50" height="50" />;
+        default:
+          return null;
+      }
+    });
+  };
+
+
   return (
-    <div className="profile-container">
-      <div className="profile-section-left">
-        <div className="profile-picture">
+    <div className="travel-card-container">
+      <div className="travel-card-left">
+        <div className="travel-card-profile-picture">
           <img src={profileData.profilePicture || defaultProfileImage} alt="Profile" />
         </div>
-        <div className="profile-data-left">
-          <p>First Name: {profileData.firstName}</p>
-          <p>Last Name: {profileData.lastName}</p>
-          <p>Age: {profileData.age}</p>
+        <div className="travel-card-data-left">
+          <p id="travel-card-name">{profileData.firstName}, {profileData.age}</p>
+          <p id="travel-card-budget">Budget: {profileData.budget}/day</p>
         </div>
       </div>
-      <div className="profile-section-right">
-        <div className="bio-section">
+      <div className="travel-card-right">
+        <div className="travel-card-bio">
           <p>Bio: {profileData.bio}</p>
         </div>
-        <div className="budget-section">
-          <p>Budget/Day: {profileData.budget}</p>
-        </div>
-        <div className="interests-section">
-          <p>Interests: {profileData.interests.join(', ')}</p>
+
+        <div className="travel-card-interests">
+          {renderInterestImages()}
         </div>
       </div>
     </div>
--- journey-joiner/src/pages/Profile.jsx
+++ journey-joiner/src/pages/Profile.jsx
@@ -86,9 +86,11 @@ export default function Profile() {
                 onChange={(e) => setProfileData({ ...profileData, budget: e.target.value })}
                 >
                     <option value="">Select Budget</option>
-                    <option value="<$100">&lt;$100</option>
-                    <option value="<$300">&lt;$300</option>
-                    <option value="<$1000">&lt;$1000</option>
+                    <option value="$100">$100</option>
+                    <option value="$200">$200</option>
+                    <option value="$300">$300</option>
+                    <option value="$500">$500</option>
+                    <option value="$1000">$1000</option>
                 </select>
             </div>
 
