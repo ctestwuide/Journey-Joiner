fb1fde137ff109c046432a394c22e5ad4000a3f0
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-03 21:25:19 -0500

    profile page styling

--- journey-joiner/src/pages/Profile.jsx
+++ journey-joiner/src/pages/Profile.jsx
@@ -1,130 +1,170 @@
 import React, { useState } from 'react';
 import Dashboard from "../components/Dashboard"
+import defaultProfileImage from "../assets/blank-profile.png"
 
 export default function Profile() {
   const [profileData, setProfileData] = useState({
+    profilePicture: null,
     firstName: '',
     lastName: '',
     age: '',
     bio: '',
     budget: '',
     interests: [],
-    email: '',
-    password: '',
   });
 
-  const handleChange = (e) => {
-    setProfileData({
-      ...profileData,
-      [e.target.name]: e.target.value,
-    });
+  const handleProfilePictureChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setProfileData({ ...profileData, profilePicture: URL.createObjectURL(file) });
+    }
   };
 
-  const handleInterestsChange = (e) => {
-    const { options } = e.target;
-    const selectedInterests = Array.from(options)
-      .filter((option) => option.selected)
-      .map((option) => option.value);
-    setProfileData({
-      ...profileData,
-      interests: selectedInterests,
-    });
+  const handleInterestClick = (interest) => {
+    const updatedInterests = profileData.interests.includes(interest)
+      ? profileData.interests.filter((item) => item !== interest)
+      : [...profileData.interests, interest];
+    setProfileData({ ...profileData, interests: updatedInterests });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you would usually send the updated profileData to the backend
+    // Submit the profile data to the backend
     console.log(profileData);
   };
 
   return (
+    <>
     <main className="main-background">
-        <Dashboard />
-        <div className="profile-container">
-        <div className="left-section">
-            <div className="profile-picture"></div>
-            <input
-                type="text"
-                name="firstName"
-                value={profileData.firstName}
-                onChange={handleChange}
-                placeholder="First Name"
-                required
-            />
-            <input
-                type="text"
-                name="lastName"
-                value={profileData.lastName}
-                onChange={handleChange}
-                placeholder="Last Name"
-                required
-            />
-            <input
-                type="number"
-                name="age"
-                value={profileData.age}
-                onChange={handleChange}
-                placeholder="Age"
-                required
-            />
+    <Dashboard />
+    <div className="profile-container">
+        <div className="profile-section-left">
+            <div className="profile-picture">
+                <img src={profileData.profilePicture || defaultProfileImage} alt="Profile" />
+                <input type="file" accept="image/*" onChange={handleProfilePictureChange} />
+            </div>
+            <div className="profile-data-left">
+                <input
+                    type="text"
+                    name="firstName"
+                    value={profileData.firstName}
+                    onChange={(e) => setProfileData({ ...profileData, firstName: e.target.value })}
+                    placeholder="First Name"
+                />
+                <input
+                    type="text"
+                    name="lastName"
+                    value={profileData.lastName}
+                    onChange={(e) => setProfileData({ ...profileData, lastName: e.target.value })}
+                    placeholder="Last Name"
+                />
+                <input
+                    type="number"
+                    name="age"
+                    value={profileData.age}
+                    onChange={(e) => setProfileData({ ...profileData, age: e.target.value })}
+                    placeholder="Age"
+                />
+            </div>
         </div>
-        <div className="right-section">
-          <textarea
-            name="bio"
-            value={profileData.bio}
-            onChange={handleChange}
-            placeholder="Bio"
-            required
-          />
-          <div className="budget-section">
-            <label>Budget per day:</label>
-            <select name="budget" value={profileData.budget} onChange={handleChange}>
-              <option value="<$100">&lt;$100</option>
-              <option value="<$300">&lt;$300</option>
-              <option value="<$1000">&lt;$1000</option>
-            </select>
-          </div>
-          <div className="interests-section">
-            <label>Interests:</label>
-            <select
-              name="interests"
-              multiple
-              value={profileData.interests}
-              onChange={handleInterestsChange}
-            >
-              <option value="beach bum">Beach Bum</option>
-              <option value="foodie">Foodie</option>
-              <option value="adventurer">Adventurer</option>
-              <option value="museum magnet">Museum Magnet</option>
-            </select>
-          </div>
+
+        <div className="profile-section-right">
+            <div className="bio-section">
+                <textarea
+                name="bio"
+                value={profileData.bio}
+                onChange={(e) => setProfileData({ ...profileData, bio: e.target.value })}
+                placeholder="Bio"
+                />
+            </div>
+
+            <div className="budget-section">
+                <label>Budget/Day:</label>
+                <select
+                name="budget"
+                value={profileData.budget}
+                onChange={(e) => setProfileData({ ...profileData, budget: e.target.value })}
+                >
+                    <option value="">Select Budget</option>
+                    <option value="<$100">&lt;$100</option>
+                    <option value="<$300">&lt;$300</option>
+                    <option value="<$1000">&lt;$1000</option>
+                </select>
+            </div>
+
+            <div className="interests-section">
+                <label>Interests:</label>
+                <div className="interests-buttons">
+                    <button
+                        type="button"
+                        className={profileData.interests.includes('beach bum') ? 'selected' : ''}
+                        onClick={() => handleInterestClick('beach bum')}
+                    >
+                    Beach Bum
+                    </button>
+                    <button
+                        type="button"
+                        className={profileData.interests.includes('foodie') ? 'selected' : ''}
+                        onClick={() => handleInterestClick('foodie')}
+                    >
+                    Foodie
+                    </button>
+                    <button
+                        type="button"
+                        className={profileData.interests.includes('adventurer') ? 'selected' : ''}
+                        onClick={() => handleInterestClick('adventurer')}
+                    >
+                    Adventurer
+                    </button>
+                    <button
+                        type="button"
+                        className={profileData.interests.includes('museum magnet') ? 'selected' : ''}
+                        onClick={() => handleInterestClick('museum magnet')}
+                    >
+                    Museum Magnet
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="bottom-section">
-          <div className="email-section">
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={profileData.email}
-              onChange={handleChange}
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div className="password-section">
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={profileData.password}
-              onChange={handleChange}
-              placeholder="Password"
-              required
-            />
-          </div>
-          <button type="submit" onClick={handleSubmit}>Update My Travel Card</button>
-        </div>
-      </div>
+    </div> 
+
+
+
     </main>
+    <section className="black-area">
+
+        <div className="profile-section">
+            <div className="email-password-section">
+            <div>
+                <label>Email:</label>
+                <input
+                type="email"
+                name="email"
+                value={profileData.email}
+                onChange={(e) => setProfileData({ ...profileData, email: e.target.value })}
+                placeholder="Email"
+                />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input
+                type="password"
+                name="password"
+                value={profileData.password}
+                onChange={(e) => setProfileData({ ...profileData, password: e.target.value })}
+                placeholder="Password"
+                />
+            </div>
+            </div>
+        </div>
+
+        <div className="profile-section">
+            <button type="submit" onClick={handleSubmit}>
+            Update My Travel Card
+            </button>
+        </div>
+
+    </section>
+    </>
   );
 }
