4d4ec7b98210f4000a274fff1e93ae56f3124c7c
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-04 19:48:16 -0500

    styling scroll bar for matches container

--- journey-joiner/src/components/TravelCardSmall.jsx
+++ journey-joiner/src/components/TravelCardSmall.jsx
@@ -7,45 +7,42 @@ import museumMagnetImage from "../assets/museum-magnet.png"
 
 
 export default function TravelCardSmall({ profileData }) {
-
-  const renderInterestImages = () => {
-    return profileData.interests.map((interest) => {
-      switch (interest) {
-        case 'beach bum':
-          return <img src={beachBumImage} alt="Beach Bum" width="15" height="15" />;
-        case 'foodie':
-          return <img src={foodieImage} alt="Foodie" width="15" height="15" />;
-        case 'adventurer':
-          return <img src={adventurerImage} alt="Adventurer" width="15" height="15" />;
-        case 'museum magnet':
-          return <img src={museumMagnetImage} alt="Museum Magnet" width="15" height="15" />;
-        default:
-          return null;
-      }
-    });
-  };
-
-
-  return (
-    <div className="travel-card-small-container" key={profileData.id}>
-      <div className="travel-card-small-left">
-        <div className="travel-card-small-profile-picture">
-          <img src={profileData.profilePicture || defaultProfileImage} alt="Profile" />
+    const renderInterestImages = () => {
+      return profileData.interests.map((interest, index) => {
+        switch (interest) {
+          case 'beach bum':
+            return <img key={index} src={beachBumImage} alt="Beach Bum" width="15" height="15" />;
+          case 'foodie':
+            return <img key={index} src={foodieImage} alt="Foodie" width="15" height="15" />;
+          case 'adventurer':
+            return <img key={index} src={adventurerImage} alt="Adventurer" width="15" height="15" />;
+          case 'museum magnet':
+            return <img key={index} src={museumMagnetImage} alt="Museum Magnet" width="15" height="15" />;
+          default:
+            return null;
+        }
+      });
+    };
+  
+    return (
+      <div className="travel-card-small-container">
+        <div className="travel-card-small-left">
+          <div className="travel-card-small-profile-picture">
+            <img src={profileData.profilePicture || defaultProfileImage} alt="Profile" />
+          </div>
+          <div className="travel-card-small-data-left">
+            <p id="travel-card-small-name">{profileData.firstName}, {profileData.age}</p>
+            <p id="travel-card-small-budget">Budget: {profileData.budget}/day</p>
+          </div>
         </div>
-        <div className="travel-card-small-data-left">
-          <p id="travel-card-small-name">{profileData.firstName}, {profileData.age}</p>
-          <p id="travel-card-small-budget">Budget: {profileData.budget}/day</p>
-        </div>
-      </div>
-      <div className="travel-card-small-right">
-        <div className="travel-card-small-bio">
-          <p>Bio: {profileData.bio}</p>
-        </div>
-
-        <div className="travel-card-small-interests">
-          {renderInterestImages()}
+        <div className="travel-card-small-right">
+          <div className="travel-card-small-bio">
+            <p>Bio: {profileData.bio}</p>
+          </div>
+          <div className="travel-card-small-interests">
+            {renderInterestImages()}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
--- journey-joiner/src/pages/Matches.jsx
+++ journey-joiner/src/pages/Matches.jsx
@@ -6,18 +6,68 @@ import defaultProfileImage from '../assets/blank-profile.png';
 import logo from '../assets/logo.png';
 
 export default function Matches() {
-  const profileData = {
-    id: "1",
-    profilePicture: defaultProfileImage,
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    budget: '$300',
-    interests: ['beach bum', 'foodie', 'adventurer', 'museum magnet'],
-  };
+  const profiles = [
+    {
+      id: "1",
+      profilePicture: defaultProfileImage,
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+      budget: '$300',
+      interests: ['beach bum', 'foodie', 'adventurer', 'museum magnet'],
+    },
+    {
+        id: "2",
+        profilePicture: defaultProfileImage,
+        firstName: 'Joe',
+        lastName: 'Doe',
+        age: 22,
+        bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        budget: '$300',
+        interests: ['beach bum', 'adventurer', 'museum magnet'],
+      },
+      {
+        id: "3",
+        profilePicture: defaultProfileImage,
+        firstName: 'Lindsey',
+        lastName: 'Doe',
+        age: 24,
+        bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        budget: '$300',
+        interests: ['beach bum', 'adventurer', 'museum magnet'],
+      },
+      {
+        id: "4",
+        profilePicture: defaultProfileImage,
+        firstName: 'Marius',
+        lastName: 'Doe',
+        age: 28,
+        bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        budget: '$300',
+        interests: ['beach bum', 'adventurer', 'museum magnet'],
+      },
+      {
+        id: "5",
+        profilePicture: defaultProfileImage,
+        firstName: 'Tom',
+        lastName: 'Doe',
+        age: 53,
+        bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        budget: '$300',
+        interests: ['beach bum', 'adventurer', 'museum magnet'],
+      },
+  ];
 
   const [selectedProfile, setSelectedProfile] = useState(null);
 
@@ -25,68 +75,33 @@ export default function Matches() {
     console.log('Clicked profile:', profile);
     setSelectedProfile({ ...profile });
   };
-  
-  
 
   return (
     <main className="main-background">
       <Dashboard />
       <div className="matches-messages-container">
         <section className="matches-container">
-            <div className="matches-container-title">
-                <img src={logo} alt="Journey Joiner Logo" width="40px" />
-                <h2>Matches</h2>
-            </div>
-
-            <div className="matches-container-profiles">
-                <TravelCardMini
-                    key="1"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-                <TravelCardMini
-                    key="2"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-                                <TravelCardMini
-                    key="3"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-                                <TravelCardMini
-                    key="4"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-                                <TravelCardMini
-                    key="5"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-                                <TravelCardMini
-                    key="6"
-                    profileData={profileData}
-                    className="matches-container-profile"
-                    onClick={() => handleProfileClick(profileData)}
-                />
-
+          <div className="matches-container-title">
+            <img src={logo} alt="Journey Joiner Logo" width="40px" />
+            <h2>Matches</h2>
+          </div>
+          <div className="matches-container-profiles">
+            {profiles.map((profile) => (
+              <TravelCardMini
+                key={profile.id}
+                profileData={profile}
+                className="matches-container-profile"
+                onClick={() => handleProfileClick(profile)}
+              />
+            ))}
           </div>
         </section>
-
         <section className="messages-container">
           <div className="matches-container-title">
             <img src={logo} alt="Journey Joiner Logo" width="40px" />
             <h2>Messages</h2>
           </div>
-          {selectedProfile && (
-            <TravelCardSmall profileData={selectedProfile} />
-          )}
+          {selectedProfile && <TravelCardSmall profileData={selectedProfile} />}
           <div className="direct-messaging">{/* Direct messaging component */}</div>
         </section>
       </div>
