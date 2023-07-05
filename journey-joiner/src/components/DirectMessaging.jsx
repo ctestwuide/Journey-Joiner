e0368e40db3dc0ab3558535340a434398b36c2ad
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-04 21:01:36 -0500

    Added direct messaging styles

--- journey-joiner/src/components/DirectMessaging.jsx
+++ journey-joiner/src/components/DirectMessaging.jsx
@@ -8,18 +8,42 @@ export default function DirectMessaging() {
     // Update the messages state using setMessages
   }, []);
 
+  const fetchedMessages = [
+    // Fetched messages from the database
+  ];
+
+  const manuallyCreatedMessages = [
+    {
+      id: 1,
+      sender: 'John',
+      content: 'Hey, how are you?',
+    },
+    {
+      id: 2,
+      sender: 'Jane',
+      content: 'I\'m good! How about you?',
+    },
+    // Add more messages here...
+  ];
+
+  const allMessages = [...fetchedMessages, ...manuallyCreatedMessages];
+
+  const currentUser = 'John'; // Assuming the current user is 'John'
+  
   return (
     <div className="direct-messaging">
       <div className="message-list">
-        {messages.map((message) => (
-          <div key={message.id}>
-            {/* Render the message content */}
-            <p>{message.messageContent}</p>
+        {allMessages.map((message) => (
+          <div
+            key={message.id}
+            className={`message-${message.sender === currentUser ? 'sent' : 'received'}`}
+          >
+            <div>{message.content}</div>
           </div>
         ))}
       </div>
       <div className="message-input">
-        <input type="text" placeholder="Type your message..." />
+        <input type="text" placeholder="Type your message" />
         <button>Send</button>
       </div>
     </div>
