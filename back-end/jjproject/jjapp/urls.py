211d9c2860adcb17e9b57df98357fd68ff295cb9
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-06 22:21:26 -0500

    added signup url

--- back-end/jjproject/jjapp/urls.py
+++ back-end/jjproject/jjapp/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'jjapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Example URL pattern
-    # Add more URL patterns for your app views
+    path('signup/', views.signup, name='signup'),
 ]
