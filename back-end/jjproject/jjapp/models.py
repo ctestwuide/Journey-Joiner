6c419a94b53cad9f25ad647299e44b9008e63b36
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-06 21:35:18 -0500

    updated models for back-end

--- back-end/jjproject/jjapp/models.py
+++ back-end/jjproject/jjapp/models.py
@@ -1,30 +1,50 @@
 from django.db import models
 
+# Custom User model
 class User(models.Model):
-    UserID = models.AutoField(primary_key=True)
-    FirstName = models.CharField(max_length=100)
-    LastName = models.CharField(max_length=100)
-    BirthDate = models.DateField()
-    Password = models.CharField(max_length=100)
-    Email = models.EmailField()
-    ProfilePicture = models.ImageField(upload_to='profile_pics/')
-    Bio = models.TextField()
-    Budget = models.DecimalField(max_digits=8, decimal_places=2)
-    Interests = models.TextField()
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    bio = models.TextField()
+    budget = models.CharField(max_length=50)
+    interests = models.ManyToManyField('Interest')
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+# Model to represent user interests
+class Interest(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Model to represent user likes
+class Like(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_likes')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_likes')
+
+    def __str__(self):
+        return f'{self.sender} likes {self.receiver}'
+
+# Model to represent matches
 class Match(models.Model):
-    user1 = models.ForeignKey(User, related_name='user1_matches', on_delete=models.CASCADE)
-    user2 = models.ForeignKey(User, related_name='user2_matches', on_delete=models.CASCADE)
-    is_matched = models.BooleanField(default=False)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1_matches')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2_matches')
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user1', 'user2'], name='unique_match')
-        ]
+    def __str__(self):
+        return f'{self.user1} matched with {self.user2}'
 
+# Model to represent messages
 class Message(models.Model):
-    MessageID = models.AutoField(primary_key=True)
-    Sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    Receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
-    MessageContent = models.TextField()
-    Timestamp = models.DateTimeField(auto_now_add=True)
+    message_id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    message_content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} to {self.receiver}: {self.message_content}'
+
