e6679de7ee5177c49f6e204a9c9890f3a57074fd
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-07-14 08:33:30 -0500

    unit testing

--- back-end/jjproject/api/tests.py
+++ back-end/jjproject/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import User, Like, Pass, Match, Message
+from .views import getUsers, getUser, signup, login, updateUser, updateUserPicture, getUnseenUsers, pass_user, like_user, getMatches
+from rest_framework.test import APIRequestFactory, force_authenticate
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            first_name="test",
+            last_name="user",
+            age=25,
+            email="testuser@gmail.com",
+            password="test123"
+        )
+
+
+
